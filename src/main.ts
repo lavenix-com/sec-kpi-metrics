1cac8fe9b830f4fd8476915a37626ecd768e7248
baalmor <alexey@lavenix.com>
2025-10-08 15:28:58 +0200

    initial

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import './assets/tailwind.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import VueGtag from 'vue-gtag'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import font awesome icons */
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fas)
+
+const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.use(router)
+
+app.mount('#app')
